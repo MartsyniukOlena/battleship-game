3175b4cf1b5f8aeb55ffb841002809320b9804b7
MartsyniukOlena <>
2023-11-29 11:42:07 +0000

    Add code to colorize symbols for missed shots and hit ships

--- run.py
+++ run.py
@@ -8,3 +8,13 @@ def clear_screen():
     Clears the termina screen.
     """
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def colorize_cell(cell):
+    """
+    Colorizes the symbols.
+    """
+    colors = {
+        '-': '\033[91m',  # Red for missed shots
+        'X': '\033[92m',  # Green for hit ships
+    }
+    return f"{colors.get(cell, '')}{cell}\033[0m"
