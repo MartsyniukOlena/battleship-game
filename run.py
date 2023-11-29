ba4c6fd93b71ea5675710577ad2bda0b91870701
MartsyniukOlena <>
2023-11-29 11:34:05 +0000

    Add code to clear terminal screen for a new game

--- run.py
+++ run.py
@@ -2,3 +2,9 @@ import os
 import random
 from colorama import Fore, Style
 
+
+def clear_screen():
+    """
+    Clears the termina screen.
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
