9198591bb9b21bff4c7fbae5321dc1f50f58bfd8
MartsyniukOlena <>
2023-11-29 12:19:21 +0000

    Add code to create random ship position that has not been used

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ def clear_screen():
     """
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
 def colorize_cell(cell):
     """
     Colorizes the symbols.
@@ -19,6 +20,8 @@ def colorize_cell(cell):
     }
     return f"{colors.get(cell, '')}{cell}\033[0m"
 
+
+
 def print_game_board(player_board, computer_board):
     """
     Prints the game board for both the player and the computer.
@@ -28,7 +31,20 @@ def print_game_board(player_board, computer_board):
     for i, (player_row, computer_row) in enumerate(zip(player_board, computer_board), start=1):
         print(f"{i} | {' '.join(colorize_cell(cell) for cell in player_row)}    |    {i} | {' '.join(colorize_cell(cell) for cell in computer_row)}")
 
-    
+
+def create_random_ship(used_positions):
+    """
+    Creates a random ship position that is not used.
+    It keeps track of ship positions already generated on the game board,
+    and creates a random ship position that hasn't been used before.
+    """
+    while True:
+        ship_position = (random.randint(0, 4), random.randint(0, 4))
+        if ship_position not in used_positions:
+            used_positions.add(ship_position)
+            return ship_position
+
+
 def play_game():
     """
     Plays the battleship game.
